4b15b5e373ae1758640df8b5cf59b80716b4f9a2
Vicky Kumar Xaxa <vickykumar835302@gmail.com>
2022-06-22 10:51:11 +0530

    updated

--- message_app/lib/main.dart
+++ message_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/personal_message.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Message',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Message'),
-        ),
-      ),
+      home: PersonalMessage(),
     );
   }
 }
